afd1e801c0604134404084d2ab33b1eb268f7e68
Fran <fmvilas@gmail.com>
2015-01-31 18:11:57 +0100

    Adding new tests

--- test/templo.js
+++ test/templo.js
@@ -1,40 +1,88 @@
 /*global describe,beforeEach,it*/
+
+/*
+ * TODO: Missing tests for types and default types.
+ */
 'use strict';
 
 var assert = require('assert'),
-  templo = require('../lib/json-tpl.js'),
-  user_template,
-  user_data;
+    templo = require('../lib/templo.js'),
+    user_show_template,
+    user_create_template,
+    user_data,
+    result;
 
-describe('templo', function() {
+describe('TemploJS', function() {
   beforeEach(function() {
-    user_template = {
-      id:         { type: 'number' },
-      name:       { type: 'string' },
-      avatar_url: { type: 'string', required: true }
+    user_show_template = {
+      id:         { type: 'string', required: true },
+      email:      { type: 'string', required: true },
+      name:       { type: 'string', required: true },
+      avatar_url: { type: 'string', default: '' },
+      created_at: { type: 'string', required: true },
+      updated_at: { type: 'string', required: true }
+    };
+
+
+    user_create_template = {
+      email:      { type: 'string', required: true },
+      password:   { type: 'string', required: true },
+      name:       { type: 'string', required: true },
+      avatar_url: { type: 'string', default: null },
+      created_at: { type: 'string', read_only: true, default: 'timestamp' },
+      updated_at: { type: 'string', read_only: true, default: 'timestamp' }
     };
 
     user_data = {
-      id: 1,
       email: 'fake@email.com',
-      name: 'Fake',
-      created_at: '2014-10-24T20:39:12Z',
-      updated_at: '2014-10-25T21:09:02Z'
+      name: 'Fake'
     };
   });
 
-  describe('#parse', function() {
-    it('should return only id, name and avatar_url', function() {
-      var output = templo.parse(user_template, user_data);
+  describe('#render', function() {
+    it('should return an object filtered by the template', function() {
+      user_data.id = '1';
+      user_data.created_at = '2014-10-24T20:39:12Z';
+      user_data.updated_at = '2014-10-25T21:09:02Z';
+      user_data.it_should_not_be_here = 'It shouldnt be here';
 
-      console.dir(output);
+      result = templo.render(user_show_template, user_data);
 
-      assert.strictEqual(output.id, 1);
-      assert.strictEqual(output.name, 'Fake');
-      assert.strictEqual(output.avatar_url, null);
-      assert.strictEqual(output.email, undefined);
-      assert.strictEqual(output.created_at, undefined);
-      assert.strictEqual(output.updated_at, undefined);
+      assert.strictEqual(result.status, 'ok');
+      assert.strictEqual(result.output.id, user_data.id);
+      assert.strictEqual(result.output.name, user_data.name);
+      assert.strictEqual(result.output.avatar_url, user_show_template.avatar_url.default);
+      assert.strictEqual(result.output.email, user_data.email);
+      assert.strictEqual(result.output.created_at, user_data.created_at);
+      assert.strictEqual(result.output.updated_at, user_data.updated_at);
+      assert.strictEqual(result.output.it_should_not_be_here, undefined);
+    });
+
+    it('should warn about read only params', function() {
+      user_data.password = 'password here';
+      user_data.created_at = '2014-10-24T20:39:12Z';
+      user_data.updated_at = '2014-10-25T21:09:02Z';
+      user_data.it_should_not_be_here = 'It shouldnt be here but will not be warned';
+
+      result = templo.render(user_create_template, user_data);
+
+      assert.strictEqual(result.status, 'ok');
+      assert.strictEqual(typeof result.warnings, 'object');
+      assert.strictEqual(result.warnings.created_at.message, 'Can\'t modify read only attribute <created_at>.');
+      assert.strictEqual(result.warnings.updated_at.message, 'Can\'t modify read only attribute <updated_at>.');
+    });
+
+    it('should fail if a required attribute is not passed', function() {
+      result = templo.render(user_create_template, user_data);
+
+      assert.strictEqual(result.status, 'error');
+      assert.strictEqual(result.errors.password.message, 'Missing required attribute <password>.');
+    });
+
+    it('should respond with warnings as false if there is no warnings', function() {
+      result = templo.render(user_create_template, user_data);
+
+      assert.strictEqual(result.warnings, false);
     });
   });
 });
